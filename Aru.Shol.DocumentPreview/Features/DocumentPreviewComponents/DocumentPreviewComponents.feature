b128f53f65f22c8bfb844520713b277e0dc09313
Juan Barba <jbarbam@hotmail.com>
2016-01-13 17:47:16 +0100

    [Aru.Shol.DocumentPreview] - Test Document Library creation

---
+++ Aru.Shol.DocumentPreview/Features/DocumentPreviewComponents/DocumentPreviewComponents.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="393b8779-0961-4b11-8129-fcef13ed1ec7" description="Components for ARU.Shol.DocumentPreview App" featureId="393b8779-0961-4b11-8129-fcef13ed1ec7" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Aru.Shol.DocumentPreview Components" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="07f332c5-f37a-4aeb-9a80-932e064761b7" />
+    <projectItemReference itemId="4e120816-88d7-4c38-8d11-7545dd46be84" />
+    <projectItemReference itemId="859bb4bb-4e76-48e4-9db7-feea9da0db2d" />
+    <projectItemReference itemId="44c45e76-78d2-465f-93df-f6ac577d1f2f" />
+    <projectItemReference itemId="c76f1175-ea93-4923-8b99-dbe89398465a" />
+    <projectItemReference itemId="7a32c020-7348-4f0b-a48e-3f45dde5409a" />
+  </projectItems>
+</feature>
---
+++ Aru.Shol.DocumentPreview/Scripts/Aru.Shol.DocumentPreview.js
@@ -0,0 +1,47 @@
+﻿
+$(document).ready(function () {
+
+    var list = getCurrentList()
+
+});
+
+function getCurrentList(success, error) {
+    var context = SP.ClientContext.get_current();
+    var web = context.get_web();
+    var listId = _spPageContextInfo.pageListId;
+    
+    var list = web.get_lists().getById(listId);
+    context.load(list);
+
+    context.executeQueryAsync(
+     function () {
+         alert("list Url" + list.URL);
+         success(list);
+     },
+     error
+    );
+}
+
+function PreviewDocument(urlsite, urlrelativelib, iddivpreviewdoc)
+{
+
+    var strDocNameEncoded = encodeURI($('input[id^="FileLeafRef"]').val());
+    var strDocExt = $('input[id^="FileLeafRef"] + span').html();
+
+    switch (strDocExt) {
+        case ".doc":
+        case ".docx":
+        case ".xls":
+        case ".xlsx":
+        case "ppt":
+        case "pptx":
+            var strDocUrl = urlsite + "/_layouts/15/WopiFrame.aspx?sourcedoc=" + urlsite + "/" + urlrelativelib + "/" + strDocNameEncoded + strDocExt + "&action=default";
+            break;
+        default:
+            var strDocUrl = urlsite + "/" + urlrelativelib + "/" + strDocNameEncoded + strDocExt;
+            break;
+    }
+
+    var iddivpreview = "#" + iddivpreviewdoc;
+    $(iddivpreview).prop('src', strDocUrl);
+}
